d640f8509db654db8dea83fb9f023289dc90238c
Gataqua <102698501+Gataqua@users.noreply.github.com>
2024-12-07 12:23:26 -0500

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -7,7 +7,10 @@
 
 let custom_text_header = `
 <div class="custom-text-header"> <pre>
-GATACA
+  ___     __     ____     __      ___     __  
+ / __)   / _\\   (_  _)   / _\\    / __)   / _\\ 
+( (_ \\  /    \\    )(    /    \\  ( (__   /    \\
+ \___/  \\_/\\_/   (__)   \\_/\\_/   \\___)  \\_/\\_/
                                                                  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
